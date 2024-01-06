d9f7ccd30d8188e192825044aaaf9985322b2bf9
SarmithaVD <21pt27@psgtech.ac.in>
2024-01-06 12:36:15 +0530

    Level0 Optimised

--- SouceFiles/level0.py
+++ SouceFiles/level0.py
@@ -1,6 +1,6 @@
 import json
 import numpy as np
-from python_tsp.heuristics import solve_tsp_local_search
+from python_tsp.exact import solve_tsp_dynamic_programming
 
 f = open("InputData\\level0.json")
 data = json.load(f)
@@ -12,6 +12,7 @@ n_restaurants = data["n_restaurants"]
 dist_matrix = []
 dist = [0]
 dist.extend(data["restaurants"]["r0"]["neighbourhood_distance"])
+dist_matrix.append(dist)
 
 i = 0
 for n in data["neighbourhoods"]:
@@ -21,13 +22,15 @@ for n in data["neighbourhoods"]:
     dist_matrix.append(dist)
 
 distance_matrix = np.array(dist_matrix)
+print(distance_matrix)
 
-permutation, distance = solve_tsp_local_search(distance_matrix)
+permutation, distance = solve_tsp_dynamic_programming(distance_matrix)
 opDict = {}
 pathDict = {}
 path = ["r0"]
 for n in permutation:
-    path.append("n" + str(n))
+    if n != 20:
+        path.append("n" + str(n))
 path.append("r0")
 pathDict["path"] = path
 opDict["v0"] = pathDict
